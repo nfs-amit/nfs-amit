fa27d43228e192d4f6d8922ccb1f8a548b88ec3e
AMIT KUMAR <146472858+nfs-amit@users.noreply.github.com>
2025-03-08 13:32:05 +0530

    Update invertedHalfPyramid_withNo.java

--- src/invertedHalfPyramid_withNo.java
+++ src/invertedHalfPyramid_withNo.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class invertedHalfPyramid_withNo {
+public class invertedHalfPyramid_withNo {
+    public static void main(String[]args){
+        int n= 5;
+        //outer loop
+        for (int i =1; i<=n; i++){
+            //inner loop
+            for (int j = 1; j<=n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+
+
+//output
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
